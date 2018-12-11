9f3de59f2f28fa8b557050c158fd0340532f733f
supertuba12 <hampus.erikssonco@live.se>
2018-12-11 19:03:44 +0100

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Snow from 'react-snow-effect';
+import SnowStorm from 'react-snowstorm';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="http://res.publicdomainfiles.com.s3.amazonaws.com/pdf_alternate/57/13529242816467.svg?AWSAccessKeyId=AKIAJBE24BKMOLMJBBXA&Expires=1544551784&Signature=5N22jLtwFXQHxTXwkdXDk8W6GhE%3D" className="App-logo" alt="logo" />
+          <p> Welcome to Santa's (Lithe kod's) secret wish site. </p>
+          <p> Please make a wish</p>
+          <a
+            className="App-link"
+            href="http://lithekod.se/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lithe Jul
+          </a>
+        </header>
+        <SnowStorm />
+      </div>
+    );
+  }
+}
+
+export default App;
