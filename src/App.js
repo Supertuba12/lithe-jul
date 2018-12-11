15a105f313671fa7a373ca2b57b57eda8856f170
Alexander Carlström <alexander@Alexanders-MacBook-Pro.local>
2018-12-11 19:49:07 +0100

    added lithekod image

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <img src={require('./lithekod.png')} className="lithekod" alt="lithekod" />
         <header className="App-header">
           <img src="http://res.publicdomainfiles.com.s3.amazonaws.com/pdf_alternate/57/13529242816467.svg?AWSAccessKeyId=AKIAJBE24BKMOLMJBBXA&Expires=1544551784&Signature=5N22jLtwFXQHxTXwkdXDk8W6GhE%3D" className="App-logo" alt="logo" />
           <p> Welcome to Santa's (Lithe kod's) secret wish site. </p>
